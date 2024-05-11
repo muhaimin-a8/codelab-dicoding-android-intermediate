d7ecd1339f16c693eda956a4b882c50d65887d23
Muhaimin Abdillah <muhaiminabdillah85@gmail.com>
2024-05-11 17:29:08 +0700

    FEAT finish codelab stack widget

---
+++ app/src/main/java/me/muhaimin/mystackwidget/StackWidgetService.kt
@@ -0,0 +1,10 @@
+package me.muhaimin.mystackwidget
+
+import android.content.Intent
+import android.widget.RemoteViewsService
+
+class StackWidgetService: RemoteViewsService() {
+    override fun onGetViewFactory(intent: Intent?): RemoteViewsFactory {
+        return StackRemoteViewsFactory(this.applicationContext)
+    }
+}
