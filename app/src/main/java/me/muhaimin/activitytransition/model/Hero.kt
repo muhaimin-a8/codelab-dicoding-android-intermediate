9480b57598360fae66598ecbd5081b7ff4027f05
Muhaimin Abdillah <muhaiminabdillah85@gmail.com>
2024-05-12 20:27:37 +0700

    FEAT add project starter

---
+++ app/src/main/java/me/muhaimin/activitytransition/model/Hero.kt
@@ -0,0 +1,16 @@
+package me.muhaimin.activitytransition.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+
+/**
+ * Created by sidiqpermana on 10/29/16.
+ */
+
+@Parcelize
+data class Hero(
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable
