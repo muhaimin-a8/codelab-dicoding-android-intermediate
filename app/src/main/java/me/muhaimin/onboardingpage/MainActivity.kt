92e2fdb7cb6aad0f45e0554c2a5f73524e1445cf
Muhaimin Abdillah <muhaiminabdillah85@gmail.com>
2024-05-19 10:31:13 +0700

    finish codelab animation with motion layout

---
+++ app/src/main/java/me/muhaimin/onboardingpage/MainActivity.kt
@@ -0,0 +1,28 @@
+package me.muhaimin.onboardingpage
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
+}
