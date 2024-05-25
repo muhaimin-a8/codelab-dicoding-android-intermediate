19781e50ffa6d29f10c102fb35115bf66c98f750
Muhaimin Abdillah <muhaiminabdillah85@gmail.com>
2024-05-25 10:34:05 +0700

    finish codelab background service

---
+++ app/src/main/java/me/muhaimin/myservice/MainActivity.kt
@@ -0,0 +1,23 @@
+package me.muhaimin.myservice
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.muhaimin.myservice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val serviceIntent = Intent(this, MyBackgroundService::class.java)
+        binding.btnStartBackgroundService.setOnClickListener {
+            startService(serviceIntent)
+        }
+        binding.btnStopBackgroundService.setOnClickListener {
+            stopService(serviceIntent)
+        }
+    }
+}
