d5acdbb09876718672c3845b3f328bbff214a338
Muhaimin Abdillah <muhaiminabdillah85@gmail.com>
2024-05-25 22:23:24 +0700

    finish codelab bound servce

--- app/src/main/java/me/muhaimin/myservice/MainActivity.kt
+++ app/src/main/java/me/muhaimin/myservice/MainActivity.kt
@@ -1,11 +1,14 @@
 package me.muhaimin.myservice
 
 import android.Manifest
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
@@ -23,11 +26,34 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private var boundStatus = false
+    private lateinit var boundService: MyBoundService
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val myBinder = service as MyBoundService.MyBinder
+            boundService = myBinder.getService
+            boundStatus = true
+            getNumberFromService()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            boundStatus = false
+        }
+    }
+
+    private fun getNumberFromService() {
+        boundService.numberLiveData.observe(this) { number ->
+            binding.tvBoundServiceNumber.text = number.toString()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // background service
         val serviceIntent = Intent(this, MyBackgroundService::class.java)
         binding.btnStartBackgroundService.setOnClickListener {
             startService(serviceIntent)
@@ -43,6 +69,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // foreground service
         val foregroundServiceIntent = Intent(this, MyForegroundService::class.java)
         binding.btnStartForegroundService.setOnClickListener {
             if (Build.VERSION.SDK_INT >= 26) {
@@ -53,6 +80,23 @@ class MainActivity : AppCompatActivity() {
         }
         binding.btnStopForegroundService.setOnClickListener {
             stopService(foregroundServiceIntent)
+        }
+
+        // bound service
+        val boundServiceIntent = Intent(this, MyBoundService::class.java)
+        binding.btnStartBoundService.setOnClickListener {
+            bindService(boundServiceIntent, connection, BIND_AUTO_CREATE)
+        }
+        binding.btnStopBoundService.setOnClickListener {
+            unbindService(connection)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (boundStatus) {
+            unbindService(connection)
+            boundStatus = false
         }
     }
 }
