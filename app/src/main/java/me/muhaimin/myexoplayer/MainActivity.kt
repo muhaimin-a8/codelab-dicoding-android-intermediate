51c69a15e827bc3632ec19933c7eb0c74d95cdd4
Muhaimin Abdillah <muhaiminabdillah85@gmail.com>
2024-06-01 19:57:29 +0700

    codelab exo player with service

--- app/src/main/java/me/muhaimin/myexoplayer/MainActivity.kt
+++ app/src/main/java/me/muhaimin/myexoplayer/MainActivity.kt
@@ -1,5 +1,6 @@
 package me.muhaimin.myexoplayer
 
+import android.content.ComponentName
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsets
@@ -8,6 +9,9 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
+import com.google.common.util.concurrent.MoreExecutors
 import me.muhaimin.myexoplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,19 +22,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val videoItem =
-            MediaItem.fromUri("https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4")
-        val audioItem =
-            MediaItem.fromUri("https://github.com/dicodingacademy/assets/raw/main/android_intermediate_academy/bensound_ukulele.mp3")
-
-        val player = ExoPlayer.Builder(this).build().apply {
-            setMediaItem(videoItem)
-            addMediaItem(audioItem)
-            prepare()
-        }
-        binding.playerView.player = player
-
         hideSystemUI()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val sessionToken = SessionToken(this, ComponentName(this, PlaybackService::class.java))
+        val controllerFuture = MediaController.Builder(this, sessionToken).buildAsync()
+        controllerFuture.addListener({
+            binding.playerView.player = controllerFuture.get()
+        }, MoreExecutors.directExecutor())
     }
 
     private fun hideSystemUI() {
